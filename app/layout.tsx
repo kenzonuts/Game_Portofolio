89e9829fb5b061edab82743f9727e5599f672dac
Tuan Muda <tuanmuda@example.com>
2026-01-06 23:38:51 +0700

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,27 +19,11 @@ const spaceMono = Space_Mono({
 })
 
 export const metadata: Metadata = {
-  title: "Game Developer Portfolio | Player One",
+  title: "Kenzonuts - Web3 Developer Portfolio",
   description:
     "Web developer portfolio with a retro gaming theme - showcasing projects, skills, and adventures in code",
-  generator: "v0.app",
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
+ 
+
 }
 
 export default function RootLayout({
